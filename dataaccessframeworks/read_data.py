cb3a62bd10b9af59bd82b064e66fbe347f950f3b
Baron <04170133@gm.scu.edu.tw>
2021-11-12 01:41:29 +0800

    To train model 1

--- dataaccessframeworks/read_data.py
+++ dataaccessframeworks/read_data.py
@@ -7,14 +7,21 @@ def read_preprocess_data():
     df = pd.read_csv("../data/tbrain_small.csv")
     final_df = pd.read_csv("../data/需預測的顧客名單及提交檔案範例.csv")
     df = df.drop(columns=['Unnamed: 0'])
-    df = df[["chid", "shop_tag", "txn_cnt", "masts"]]
+    df = df[["dt", "chid", "shop_tag", "txn_amt", "masts"]]
 
     # simple preprocess
-    df = df[df["txn_cnt"]>0]
+    user_attributes = dict()
+    #df = df[df["txn_cnt"]>0]
     df.dropna(inplace=True)
     id_to_num = { id:i for i, id in enumerate(final_df["chid"].unique())}
     df["adj_id"] = df["chid"].map(id_to_num)
     df['masts'] = df['masts'].astype(int)
-    train = df.drop(columns=["chid"])
+    df["txn_amt_log"] = df["txn_amt"].apply(np.log)
+    # 建立使用者屬性
+    train_id = df["chid"].unique()
+    for i in final_df["chid"].unique():
+        # 若是id有存在於訓練資料中，則取得對應數值，否則為0
+        user_attributes[i] = {"masts": df[df["chid"]==i]["masts"][0] if i in train_id else 0}
+    train = df.drop(columns=["chid", "txn_amt"])
 
-    return train
+    return train, user_attributes
